a2593258864bbbecd7ec7f7ed7dd7b7173379c20
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-09-09 11:28:09 +0530

    Added code for hoist assist system

---
+++ Assets/Code/Scripts/CraneAndToolMovement/HoistAssist.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class HoistAssist : MonoBehaviour
+{
+    public Transform targetObject;
+    public float detectionRadius = 5f;
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, targetObject.position);
+        
+        if (distance <= detectionRadius)
+        {
+            Vector3 direction = (targetObject.position - transform.position).normalized;
+            DetermineDirection(direction);
+        }
+    }
+
+    void DetermineDirection(Vector3 direction)
+    {
+        if (direction.x > 0)
+        {
+            Debug.Log("Move Right");
+        }
+        else if (direction.x < 0)
+        {
+            Debug.Log("Move Left");
+        }
+
+        if (direction.y > 0)
+        {
+            Debug.Log("Move Up");
+        }
+        else if (direction.y < 0)
+        {
+            Debug.Log("Move Down");
+        }
+
+        if (direction.z > 0)
+        {
+            Debug.Log("Move Forward");
+        }
+        else if (direction.z < 0)
+        {
+            Debug.Log("Move Backward");
+        }
+    }
+}
