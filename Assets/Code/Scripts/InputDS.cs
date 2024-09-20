09d1d934fc6877c1e01c8bfbc0f678261e7a3d8a
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-09-20 17:52:08 +0530

    implemented rotary

--- Assets/Code/Scripts/InputDS.cs
+++ Assets/Code/Scripts/InputDS.cs
@@ -1,14 +1,66 @@
-[System.Serializable]
-public struct Rotary
+using System;
+[Serializable]
+public class Rotary
 {
+    public string Name;
     public int Value;
     public int MaxValue;
     public int MinValue;
 
-    public Rotary(int value, int maxValue, int minValue)
+    public Rotary(int value, int maxValue, int minValue, string name)
     {
         Value = value;
         MaxValue = maxValue;
         MinValue = minValue;
+        Name = name;
     }
+    private void SetValue(int newValue)
+    {
+        Value = Math.Clamp(newValue, MinValue, MaxValue);
+    }
+    public void RotateUp()
+    {
+        SetValue(Value + 1);
+    }
+
+    public void RotateDown()
+    {
+        SetValue(Value - 1);
+    }
+}
+
+[Serializable]
+public class PushButton
+{
+    public string Name;
+    public int Value;
+
+    public PushButton(int index, int value)
+    {
+        Name = $"Push Button {index}";
+        Value = value;
+    }
+}
+
+[Serializable]
+public class JoyStick
+{
+    public String Name;
+    public PushButton Button1;
+    public PushButton Button2;
+    public int XVal,YVal;
+
+    public JoyStick(string name)
+    {
+        Name = name;
+        Button1 = new PushButton(1,0);
+        Button2 = new PushButton(2,0);
+        XVal = 5;
+        YVal = 5;
+    }
+
+}
+public class ArmRest
+{
+
 }
--- Assets/Code/Scripts/InputHandler.cs
+++ Assets/Code/Scripts/InputHandler.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using AlligatorUtils;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
+    //TODO: Define all the input actions.
+    // Create an int array 
     public const string DEFAULT_INPUT = "550000000000000055000000";
     public static char[] InputValues = DEFAULT_INPUT.ToCharArray();
-    public Rotary   Rotary1 = new Rotary(value: 0, maxValue: 6, minValue: 0),
-                    Rotary2 = new Rotary(value: 0, maxValue: 2, minValue: 0),
-                    Rotary3 = new Rotary(value: 0, maxValue: 3, minValue: 0),
-                    Rotary4 = new Rotary(value: 0, maxValue: 3, minValue: 0);
+
+    [Header("Rotaries")]
+    public Rotary Rotary1 = new Rotary(value: 0, maxValue: 6, minValue: 0, name: "One"); 
+    public Rotary Rotary2 = new Rotary(value: 0, maxValue: 2, minValue: 0, name: "Two"); 
+    public Rotary Rotary3 = new Rotary(value: 0, maxValue: 3, minValue: 0, name: "Three"); 
+    public Rotary Rotary4 = new Rotary(value: 0, maxValue: 3, minValue: 0, name: "Four"); 
+    public Rotary ActiveRotary;
+
+    [Header("Press Buttons")]
+    public List<PushButton> RightPushButtons = new();
+    public List<PushButton> LeftPushButtons = new();
 
     #region Input
-    
+    private ControllerSetup _controllerSetup;
+    private InputAction _rotaryBtn1, _rotaryBtn2, _rotaryBtn3, _rotaryBtn4;
+    private InputAction _upShift,_downshift;
     #endregion
 
     private int ConvertJoystickValue(float axisValue)
@@ -35,5 +48,49 @@ public class InputHandler : MonoBehaviour
             return 8;
         else
             return 9;
+    }
+
+    private void Awake()
+    {
+        _controllerSetup = new ControllerSetup();
+        ControllerSetup.ControlsActions controls = _controllerSetup.Controls;
+        SetActiveRotary(Rotary1);
+
+        _rotaryBtn1 = controls.Rotary1;
+        _rotaryBtn2 = controls.Rotary2;
+        _rotaryBtn3 = controls.Rotary3;
+        _rotaryBtn4 = controls.Rotary4;
+
+        _upShift = controls.UpShift;
+        _downshift = controls.DownShift;
+
+        _rotaryBtn1.performed += ctx => SetActiveRotary(Rotary1);
+        _rotaryBtn2.performed += ctx => SetActiveRotary(Rotary2);
+        _rotaryBtn3.performed += ctx => SetActiveRotary(Rotary3);
+        _rotaryBtn4.performed += ctx => SetActiveRotary(Rotary4);
+
+        _upShift.performed += ctx => 
+        {
+           ActiveRotary.RotateUp();
+        };
+
+        _downshift.performed += ctx => 
+        {
+            ActiveRotary.RotateDown();
+        };
+    }
+
+    private void SetActiveRotary(Rotary rotary)
+    {
+        ActiveRotary = rotary;
+    }
+
+    public void OnEnable()
+    {
+        _controllerSetup.Controls.Enable();
+    }
+    public void OnDisable()
+    {
+        _controllerSetup.Controls.Disable();
     }
 }
